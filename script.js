6581a9fae7ea117962108c93c55dfd47cbac7202
MayanDev <phillzou@gmail.com>
2021-09-15 14:57:10 +0800

    feat(0.0.3): support QQ & Kugou

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name        导出网易云链接
-// @description 导出网易云链接，请提前登陆网易云
+// @name        导出网易云、QQ 音乐、酷狗链接
+// @description 导出网易云、QQ 音乐链接，请提前登陆
 // @namespace   https://github.com/mayandev/export-neteast-music-link
-// @version     0.0.1
+// @version     0.0.3
 // @author      mayandev
 // @match       http*://music.163.com/*
+// @match       http*://y.qq.com/*
+// @match       http*://kugou.com/*
 // @supportURL  https://github.com/mayandev/export-neteast-music-link/issues
 // @license     GPL License
 // @run-at      document-end
@@ -22,26 +24,24 @@
     document.getElementsByTagName('head')[0].appendChild(y);
   }
 
-  function addButton() {
-    const btnCode = '<button id="export-button">导出内容</button>'
-    return $('.m-tabs.m-tabs-srch.f-cb.ztag').before(btnCode);
+  function addButton(insertDom) {
+    const btnCode = '<button id="export-button">导出内容</button>';
+    return $(insertDom).before(btnCode);
   }
 
   function convertToCSV(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var str = '';
-
-    for (var i = 0; i < array.length; i++) {
-      var line = '';
-      for (var index in array[i]) {
-        if (line != '') line += ','
-
+    let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    for (let i = 0; i < array.length; i++) {
+      let line = '';
+      for (let index in array[i]) {
+        if (line != '') {
+          line += ',';
+        }
         line += array[i][index];
       }
-
-      str += line + '\r\n';
+      str += `${line}\r\n`;
     }
-
     return str;
   }
 
@@ -50,20 +50,25 @@
       items.unshift(headers);
     }
 
-    var jsonObject = JSON.stringify(items);
-    var csv = convertToCSV(jsonObject);
+    // Convert Object to JSON
+    let jsonObject = JSON.stringify(items);
 
-    var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
-    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    let csv = convertToCSV(jsonObject);
+
+    let exportedFilenmae = `${fileTitle}.csv` || 'export.csv';
+    let blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
     if (navigator.msSaveBlob) {
+      // IE 10+
       navigator.msSaveBlob(blob, exportedFilenmae);
     } else {
-      var link = document.createElement("a");
+      let link = document.createElement('a');
       if (link.download !== undefined) {
-        var url = URL.createObjectURL(blob);
-        link.setAttribute("href", url);
-        link.setAttribute("download", exportedFilenmae);
-        link.setAttribute('target','_blank');
+        // feature detection
+        // Browsers that support HTML5 download attribute
+        let url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', exportedFilenmae);
+        link.setAttribute('target', '_blank');
         link.style.visibility = 'hidden';
         document.body.appendChild(link);
         link.click();
@@ -72,27 +77,80 @@
     }
   }
 
-  function getMusicList() {
+  function getNetEaseMusicList() {
     let list = [];
     $('.item.f-cb').each(function (index, item) {
-      const map = {};
-      const link = 'https://music.163.com/' + $(item).find('.w0').find('a').attr('href');
+      const link = `https://music.163.com/${$(item).find('.w0').find('a').attr('href')}`;
       const songName = $(item).find('.w0').find('b').attr('title');
       const artist = $(item).find('.w1').find('a').text();
       list.push({ link, songName, artist });
-    })
-    var headers = {
-      link: '歌曲链接', // remove commas to avoid errors
-      songName: '歌曲名称',
-      artist: "歌手",
-    };
-    exportCSVFile(headers,list,'export')
+    });
+    return list;
   }
 
-  addButton();
-  addStyle(`
-  #export-button { background-color: red; color: white; padding: 5px; border-radius: 2px; margin-bottom: 5px; }
-  `)
-  $('#export-button').click(getMusicList);
+  function getQQMusicList() {
+    let list = [];
+    $('.songlist__list li').each(function (index, item) {
+      const link = `https://y.qq.com/${$(item).find('.songlist__songname_txt').find('a').attr('href')}`;
+      const songName = $(item).find('.songlist__songname_txt').find('a').attr('title');
+      const artist = $(item).find('.songlist__artist').find('a').attr('title');
+      list.push({ songName, artist, link });
+    });
+    return list;
+  }
 
+  function getKugouList() {
+    let list = [];
+    $('.list_content li').each(function (index, item) {
+      const songNameAndArtist = `${$(item).find('.width_f_li').find('a').attr('title')}`;
+      const [artist, songName] = songNameAndArtist.split(' - ');
+      list.push({ songName, artist, link: '-' });
+    });
+    return list;
+  }
+
+  function init() {
+    const musicSites = [
+      {
+        name: '网易云',
+        reg: new RegExp(/.*music\.163\.com\/.+/),
+        insertDom: '.m-tabs.m-tabs-srch.f-cb.ztag',
+        exportStyle:
+          '#export-button { background-color: red; color: white; padding: 5px; border-radius: 2px; margin-bottom: 5px; }',
+        listFunc: getNetEaseMusicList,
+      },
+      {
+        name: 'QQ',
+        reg: new RegExp(/.*y\.qq\.com\/.+/),
+        insertDom: '.mod_songlist',
+        exportStyle:
+          '#export-button { background-color: #31c27c; color: white; padding: 5px; border-radius: 2px; border: 1px solid #31c27c; margin-bottom: 5px;}',
+        listFunc: getQQMusicList,
+      },
+      {
+        name: '酷狗',
+        reg: new RegExp(/.*kugou\.com\/.+/),
+        insertDom: '.song_list',
+        exportStyle:
+          '#export-button { background-color: #169af3; color: white; padding: 5px; border-radius: 2px; border: 1px solid #169af3; margin-bottom: 5px;}',
+        listFunc: getKugouList,
+      },
+    ];
+    const url = window.location.href;
+    const [site] = musicSites.filter(item => item.reg.test(url));
+    console.log(site);
+    addButton(site.insertDom);
+    addStyle(site.exportStyle);
+    $('#export-button').click(function () {
+      const list = site.listFunc();
+      const headers = {
+        link: '歌曲链接',
+        songName: '歌曲名称',
+        artist: '歌手',
+      };
+      exportCSVFile(headers, list, site.name);
+    });
+  }
+
+  setTimeout(init, 3000);
 })();
